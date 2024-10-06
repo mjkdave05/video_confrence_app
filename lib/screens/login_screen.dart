d965e8f621284ed8df42f6c83155999ecdbba92c
Majekodunmi David <majekodunmidavid05@gmail.com>
2024-10-06 23:34:03 +0100

    onboarding page

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,30 @@
+import "package:flutter/material.dart";
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            "Start or join a meeting",
+            style: const TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+            Image.asset('assets/images/onboarding.jpg'),
+
+      ],
+      ),
+    );
+  }
+}
